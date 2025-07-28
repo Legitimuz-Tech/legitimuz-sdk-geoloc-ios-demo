d3fef66f23f2d664914547ca5e0618be878c0755
Christian Santos <christian@legitimuz.com>
2025-07-28 11:45:38 -0300

    Initial Commit

---
+++ antifraude/antifraudeApp.swift
@@ -0,0 +1,17 @@
+//
+//  antifraudeApp.swift
+//  antifraude
+//
+//  Created by Christian Santos on 28/07/25.
+//
+
+import SwiftUI
+
+@main
+struct antifraudeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
